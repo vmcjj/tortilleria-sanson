65796931ffdb90ef9806908175ddfa53dd9f8600
vmcjj <vc5940804@gmail.com>
2026-02-18 23:33:36 -0800

    Create User.js model file for MongoDB user schema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    rol: { type: String, enum: ['dueño', 'vendedor', 'cliente'], required: true },
+    activo: { type: Boolean, default: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+// Pre-save hook to hash password
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
